54904e5766a329ede91d35fd266606476dd8888d
Jongseok Jung <jong03720@gmail.com>
2023-06-06 15:41:35 -0700

    feat: implement click to copy

--- components/Converter.tsx
+++ components/Converter.tsx
@@ -15,6 +15,10 @@ function Converter() {
   };
 
   const captionToEmoji = async (caption: string) => {
+    // handle when input is empty
+    if (caption === "") {
+      return;
+    }
     const emojiPrompt =
       "I want you to translate the sentences I wrote into emojis. I will write the sentence, and you will express it with emojis. I just want you to express it with emojis. I don't want you to reply with anything but emoji. When I need to tell you something in English, I will do it by wrapping it in curly brackets like {like this}";
     setLoading(true);
