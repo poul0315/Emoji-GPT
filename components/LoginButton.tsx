0c3387e84f74d240cf63c4f3a25c23675390df93
Paul Nam <poul0315@gmail.com>
2023-06-11 21:58:58 -0700

    feat(app): Added login component and the purpose of it

---
+++ components/LoginButton.tsx
@@ -0,0 +1,5 @@
+// Login set up is to help the users to store their previously achieved/written data
+// Using fireBase for login authentication
+// Using MongoDB
+
+// This would give an opportunity to learn about fireBase and MongoDB a little deeper
